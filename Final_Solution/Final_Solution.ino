b91e98585f82db45f4595ad5f54d6397ef43ca07
Alex Luehm <alex.luehm@ultimateknowledge.com>
2026-08-06 10:53:24 -0500

    chore: comment, verbose

--- Final_Solution/Final_Solution.ino
+++ Final_Solution/Final_Solution.ino
@@ -15,7 +15,7 @@ const int SW_2 = 18;
 const int SW_3 = 19;
 const int SERVO_1 = 3;
 
-// Object handles
+// Object References
 Servo servo_obj;
 
 // Global Constants
@@ -42,7 +42,7 @@ void setup() {
   pinMode(IR_1, INPUT);
   pinMode(POT_1, INPUT);
 
-  // Initialize servo
+  // Initialize servo to closed (locked) state
   servo_obj.attach(SERVO_1, 1000, 2400);
   servo_obj.write(CLOSED_POS);
 
@@ -51,14 +51,15 @@ void setup() {
 }
 
 bool test_combination(int combo){
-  // Reads value of potentiometer (value between 0 and 1023)
+  // Reads value of potentiometer (value between 0 and 511)
   int pot_val = analogRead(POT_1);
-  Serial.println("Raw Pot Value: " + String(pot_val));
+  Serial.println("\tRaw Pot Value: " + String(pot_val));
 
-  // Map potentiometer value to a value
-  // between 0 and 1
-  pot_val = map(pot_val, 0, 511, 0, 1);
-  if (pot_val == combo){
+  // Map potentiometer value to a value between 0 and 1
+  int pot_val_mapped = map(pot_val, 0, 511, 0, 1);
+  Serial.println("\tMApped Pot Value: " + String(pot_val));
+
+  if (pot_val_mapped == combo){
     return true;
   } else{
     return false;
@@ -71,7 +72,7 @@ void next_state(bool blink){
 
   // Signal success state
   // Blink LED_1 once
-  if(blink){
+  if(blink == true){
     digitalWrite(LED_1, HIGH);
     delay(250);
     digitalWrite(LED_1, LOW);
@@ -82,14 +83,30 @@ void fail_state(){
   // Reset state to start
   loop_state = 0;
 
-  // Signal failure state
-  // Blink LED_2 three times
-  for(int i = 0; i < 3; i++){
-    digitalWrite(LED_2, HIGH);
-    delay(100);
-    digitalWrite(LED_2, LOW);
-    delay(100);
-  }
+  /********************************************
+  / Signal Failure
+  / Blink LED_2 three times
+  /********************************************/
+
+  // Blink 1
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+  delay(100);
+
+  // Blink 2
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+  delay(100);
+
+  // Blink 3
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+  delay(100);
+
+  // Delay before starting again
   delay(500);
 }
 
@@ -105,15 +122,34 @@ void lid_lock(){
   // Close lid lock
   servo_obj.write(CLOSED_POS);
 
-  // Signal Lock
-  for(int i = 0; i < 3; i++){
-    digitalWrite(LED_1, HIGH);
-    delay(100);
-    digitalWrite(LED_1, LOW);
-    digitalWrite(LED_2, HIGH);
-    delay(100);
-    digitalWrite(LED_2, LOW);
-  }
+  /********************************************
+  / Signal Lock
+  / Alternate blink LED_1 and LED_2 three times
+  /********************************************/
+
+  // Blink sequence 1
+  digitalWrite(LED_1, HIGH);
+  delay(100);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+
+  // Blink sequence 2
+  digitalWrite(LED_1, HIGH);
+  delay(100);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+
+  // Blink sequence 3
+  digitalWrite(LED_1, HIGH);
+  delay(100);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
 }
 
 // The loop function runs over and over again forever
@@ -127,50 +163,68 @@ void loop() {
   Serial.println("Button: " + String(push_button));
   Serial.println("Sensor: " + String(lid_sensor));
 
+  /********************************************
+  / States 0 - 2 - Check input combination.
+  / Combination: 0, 1, 0 (L, R, L)
+  /********************************************/
+
   // state 0 -> 1 - first combo
   if (loop_state == 0){
-    if (push_button){
-      if (test_combination(0)){
+    if (push_button == 1){
+      if (test_combination(0) == true){
         next_state(true);
       } else{
         fail_state();
       }
     }
+
   // state 1 -> 2 - second combo
   } else if (loop_state == 1){
-    if (push_button){
-      if (test_combination(1)){
+    if (push_button == 1){
+      if (test_combination(1) == true){
         next_state(true);
       } else{
         fail_state();
       }
     }
+
   // state 2 -> 3 - third combo
   } else if (loop_state == 2){
-    if (push_button){
-      if (test_combination(0)){
+    if (push_button == 1){
+      if (test_combination(0) == true){
         next_state(true);
       } else{
         fail_state();
       }
     }
+
+  /********************************************
+  / States 3 - 5 - Unlock and re-lock lid
+  / Unlock lid on successful combination.
+  / After opening, wait for lid close before locking.
+  /********************************************/
+
   // state 3 -> 4 - unlock state
   } else if (loop_state == 3){
     next_state(false);
     lid_unlock();
+
   // state 4 -> 5 - lid opened
   } else if (loop_state == 4){
-    if (lid_sensor){
+    if (lid_sensor == 1){
       next_state(false);
       delay(1000);
     }
+
   // state 5 - lid lock, reset
   } else if (loop_state == 5){
-    if (!lid_sensor){
+    if (lid_sensor == 0){
       delay(1000);
       lid_lock();
       loop_state = 0;
     }
   }
+
+  // Add 1ms delay to set base loop rate;
   delay(1);
 }
