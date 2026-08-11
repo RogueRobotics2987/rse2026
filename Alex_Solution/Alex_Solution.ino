122eafe72c95b5096c49803d92fe23df81a7af19
Alex Luehm <alex.luehm@ultimateknowledge.com>
2026-08-10 21:26:43 -0500

    feat: add alex solution w/ password and tilt sensor

---
+++ Alex_Solution/Alex_Solution.ino
@@ -0,0 +1,314 @@
+#include <Servo.h>
+
+#define DEBUG 0
+
+// Output GPIO Pins
+const int LED_0   = 21;
+const int LED_1   = 0;  // Green LED
+const int LED_2   = 1;  // Red LED
+const int LED_3   = 2;
+
+// Output GPIO Pins
+const int SW_0    = 20;
+const int SW_1    = 17; // Push Button
+const int SW_2    = 18; // Tilt Switch
+const int SW_3    = 19;
+const int SERVO_1 = 3;  // Servo Lock
+const int POT_1   = 26; // Potentiometer
+const int IR_1    = 16; // Lid Sensor
+
+// Object References
+Servo lock_servo;
+
+// Global Constants
+const int CLOSED_POS = 30;
+const int OPEN_POS = 90;
+
+// Global Variables
+int loop_state = 0;
+int output_count = 0;
+
+// The setup function runs once when you press reset or power the board
+void setup() {
+  // Initialize output pins
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_0, OUTPUT);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+
+  // Initialize input pins
+  pinMode(SW_0, INPUT);
+  pinMode(SW_1, INPUT);
+  pinMode(SW_2, INPUT);
+  pinMode(SW_3, INPUT);
+  pinMode(IR_1, INPUT);
+  pinMode(POT_1, INPUT);
+
+  // Initialize servo to closed (locked) state
+  lock_servo.attach(SERVO_1, 700, 2400);
+  lock_servo.write(CLOSED_POS);
+
+  // Initialize serial port
+  Serial.begin(9600);
+}
+
+bool test_combination(int combo){
+  // Reads value of potentiometer (value between 0 and 1023)
+  int pot_val = analogRead(POT_1);
+  #ifdef DEBUG
+  Serial.println("\tRaw Pot Value: " + String(pot_val));
+  #endif 
+
+  int pot_val_mapped = 0;
+  // Map potentiometer value to a value between 0 and 1
+  if(pot_val > 511){
+    pot_val_mapped = 1;
+  }
+  #ifdef DEBUG
+  Serial.println("\tMapped Pot Value: " + String(pot_val_mapped));
+  #endif
+
+  if (pot_val_mapped == combo){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+void next_state(bool blink){
+  // Increment state to next value
+  loop_state = loop_state + 1;
+
+  // Signal success state
+  // Blink LED_1 once
+  if(blink == true){
+    digitalWrite(LED_1, HIGH);
+    delay(250);
+    digitalWrite(LED_1, LOW);
+  }
+}
+
+void fail_state(){
+  // Reset state to start
+  loop_state = 0;
+
+  /********************************************
+  / Signal Failure
+  / Blink LED_2 three times
+  /********************************************/
+
+  // Blink 1
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+  delay(100);
+
+  // Blink 2
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+  delay(100);
+
+  // Blink 3
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+  delay(100);
+
+  // Delay before starting again
+  delay(1000);
+}
+
+void lid_unlock(){
+  // Open lid lock
+  lock_servo.write(OPEN_POS);
+
+  // Signal successful open
+  digitalWrite(LED_1, HIGH);
+}
+
+void lid_lock(){
+  // Close lid lock
+  lock_servo.write(CLOSED_POS);
+
+  /********************************************
+  / Signal Lock
+  / Alternate blink LED_1 and LED_2 three times
+  /********************************************/
+
+  // Blink sequence 1
+  digitalWrite(LED_1, HIGH);
+  delay(100);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+
+  // Blink sequence 2
+  digitalWrite(LED_1, HIGH);
+  delay(100);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+
+  // Blink sequence 3
+  digitalWrite(LED_1, HIGH);
+  delay(100);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(100);
+  digitalWrite(LED_2, LOW);
+}
+
+// The loop function runs over and over again forever
+void loop() {
+
+  // Read input values
+  bool push_button = digitalRead(SW_1);
+  bool lid_sensor = digitalRead(IR_1);
+  bool tilt_sensor = !digitalRead(SW_2);
+
+  // Slow down debug output
+  #ifdef DEBUG
+  output_count = output_count + 1;
+  if (output_count == 1000){
+    output_count = 0;
+    Serial.println("===========================================");
+    Serial.println("State: " + String(loop_state));
+    Serial.println("Button: " + String(push_button));
+    Serial.println("Lid Sensor: " + String(lid_sensor));
+    Serial.println("Tilt Sensor: " + String(tilt_sensor));
+  }
+  #endif
+
+  /********************************************
+  / States 0 - 8 - Check input combination.
+  /  0  1   2   3  4   5  6   7  8
+  / (L, LT, RT, L, RT, L, LT, R, password)
+  /  NOTE: combo and tilt checked via button press
+  /********************************************/
+
+  // state 0 -> 1 - L
+  if (loop_state == 0){
+    if (push_button == 1){
+      if (test_combination(0) == true && tilt_sensor == false){
+        next_state(true);
+      } else{
+        fail_state();
+      }
+    }
+
+  // state 1 -> 2 - LT
+  } else if (loop_state == 1){
+    if (push_button == 1){
+      if (test_combination(0) == true && tilt_sensor == true){
+        next_state(true);
+      } else{
+        fail_state();
+      }
+    }
+
+  // state 2 -> 3 - RT
+  } else if (loop_state == 2){
+    if (push_button == 1){
+      if (test_combination(1) == true && tilt_sensor == true){
+        next_state(true);
+      } else{
+        fail_state();
+      }
+    }
+
+  // state 3 -> 4 - L
+  } else if (loop_state == 3){
+    if (push_button == 1){
+      if (test_combination(0) == true && tilt_sensor == false){
+        next_state(true);
+      } else{
+        fail_state();
+      }
+    }
+
+  // state 4 -> 5 - RT
+  } else if (loop_state == 4){
+    if (push_button == 1){
+      if (test_combination(1) == true && tilt_sensor == true){
+        next_state(true);
+      } else{
+        fail_state();
+      }
+    }
+
+  // state 5 -> 6 - L
+  } else if (loop_state == 5){
+    if (push_button == 1){
+      if (test_combination(0) == true && tilt_sensor == false){
+        next_state(true);
+      } else{
+        fail_state();
+      }
+    }
+
+  // state 6 -> 7 - LT
+  } else if (loop_state == 6){
+    if (push_button == 1){
+      if (test_combination(0) == true && tilt_sensor == true){
+        next_state(true);
+      } else{
+        fail_state();
+      }
+    }
+
+  // state 7 -> 8 - R
+  } else if (loop_state == 7){
+    if (push_button == 1){
+      if (test_combination(1) == true && tilt_sensor == false){
+        next_state(true);
+      } else{
+        fail_state();
+      }
+    }
+
+  // state 8 -> 9 - Password
+  } else if (loop_state == 8){
+    Serial.println("What is the first three digits of pie?");
+    while (Serial.available() == 0) {}
+    String user_input = Serial.readString();
+    user_input.trim();
+    if(user_input == "3.14"){
+      next_state(true);
+    } else {
+      fail_state();
+    }
+
+  /********************************************
+  / States 9 - 11 - Unlock and re-lock lid
+  / Unlock lid on successful combination.
+  / After opening, wait for lid close before locking.
+  /********************************************/
+
+  // state 9 -> 10 - unlock state
+  } else if (loop_state == 9){
+    next_state(false);
+    lid_unlock();
+
+  // state 10 -> 11 - lid opened
+  } else if (loop_state == 10){
+    if (lid_sensor == 1){
+      next_state(false);
+      delay(1000);
+    }
+
+  // state 11 - lid lock, reset
+  } else if (loop_state == 11){
+    if (lid_sensor == 0){
+      delay(500);
+      lid_lock();
+      loop_state = 0;
+    }
+  }
+
+  // Set base loop rate to 1ms
+  delay(1);
+}
